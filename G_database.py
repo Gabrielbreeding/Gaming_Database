222438757219b05dd5a22a4bca10b492ff4389b3
Gabriel Breeding <gbreeding@debian14.CIS.COM>
2020-01-27 08:01:18 -0600

    new file:   G_database.py

---
+++ G_database.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+#Gabriel breeding
+
+import sys
+
+
+def menu():
+    '''
+              Gaming Database Menu:
+              
+        1)   Add/Edit Game
+        2)   Print All Games
+        3)   Search by Title
+        4)   Remove a Game
+        5)   Save
+        Q)   Quit
+    '''
